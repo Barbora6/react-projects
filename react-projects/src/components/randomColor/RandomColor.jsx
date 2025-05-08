47ed15889ce8129adb6c9b96689b358ecd90f101
Barbora6 <smetanovabarbora@email.cz>
2025-05-08 16:52:38 +0200

    vytvoření tlačítek

--- react-projects/src/components/randomColor/RandomColor.jsx
+++ react-projects/src/components/randomColor/RandomColor.jsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
 export const RandomColor = () => {
-  return <div></div>;
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+  return (
+    <div>
+      <button onClick={() => setTypeOfColor("hex")}>Vytvoř HEX barvy</button>
+      <button onClick={() => setTypeOfColor("rgb")}>Vytvoř RGB barvy</button>
+    </div>
+  );
 };
