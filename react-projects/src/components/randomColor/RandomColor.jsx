ae2f8acf7aee78ca9066a865667264e2ce0bf2df
Barbora6 <smetanovabarbora@email.cz>
2025-05-08 17:32:16 +0200

    button na generování náhodného čísla

--- react-projects/src/components/randomColor/RandomColor.jsx
+++ react-projects/src/components/randomColor/RandomColor.jsx
@@ -11,6 +11,7 @@ export const RandomColor = () => {
   const handleCreateRandomHexColor = () => {
     const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     let hexColor = "#";
+
     for (let i = 0; i < 6; i++) {
       hexColor += hex[vygenerujNahodnouBarvu(hex.length)];
     }
@@ -18,10 +19,35 @@ export const RandomColor = () => {
     setColor(hexColor);
   };
 
+  const handleCreateRandomRgbColor = () => {
+    const r = vygenerujNahodnouBarvu(256);
+    const g = vygenerujNahodnouBarvu(256);
+    const b = vygenerujNahodnouBarvu(256);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  };
+
   return (
-    <div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color
+      }}
+    >
       <button onClick={() => setTypeOfColor("hex")}>Vytvoř HEX barvy</button>
       <button onClick={() => setTypeOfColor("rgb")}>Vytvoř RGB barvy</button>
+      <button
+        onClick={
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Generátor náhodné barvy
+      </button>
+
+      <h1>{color}</h1>
     </div>
   );
 };
