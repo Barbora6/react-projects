ba3e2dcf35df856f74261da609530725aae0ae34
Barbora6 <smetanovabarbora@email.cz>
2025-05-14 12:29:02 +0200

    useState, useEffect

--- react-projects/src/components/scroll-indicator/ScrollIndicator.jsx
+++ react-projects/src/components/scroll-indicator/ScrollIndicator.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
 import "./ScrollIndicator.css";
+import { useEffect } from "react";
 
-export const ScrollIndicator = () => {
+export const ScrollIndicator = ({ url }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
   return <div></div>;
 };
