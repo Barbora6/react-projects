65aba3d068400831d6482b0f10cb69a4219ab344
Barbora6 <smetanovabarbora@email.cz>
2025-05-20 22:24:22 +0200

    css scrollovani

--- react-projects/src/components/scroll-indicator/ScrollIndicator.jsx
+++ react-projects/src/components/scroll-indicator/ScrollIndicator.jsx
@@ -26,7 +26,23 @@ export const ScrollIndicator = ({ url }) => {
     fetchData(url);
   }, [url]);
 
-  const handleScrollPercentage = () => {};
+  const handleScrollPercentage = () => {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScrollPercentage);
@@ -36,9 +52,27 @@ export const ScrollIndicator = ({ url }) => {
     };
   }, []);
 
+  console.log(data, scrollPercentage);
+
+  if (errorMsg) {
+    return <div>Error ! {errorMsg}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading data ! Pleaae wait</div>;
+  }
+
   return (
     <div>
-      <h1>Indikátor posouvání</h1>
+      <div className="top-container">
+        <h1>Indikátor posouvání</h1>
+        <div className="scroll-progress-tracking-container">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
       <div className="data-container">
         {data && data.lenght > 0
           ? data.map((dataItem) => <p>{dataItem.title}</p>)
